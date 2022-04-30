854ef24a35bbcbdb0be14c413049941df87503ee
Falguni Vashishtha <shrutisharma2302@gmail.com>
2022-04-30 18:32:41 +0530

    Pattern file

---
+++ L_pattern.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+int main() {
+    
+	int i,j;
+
+	for(i=1;i<=10;i++)
+ {
+        
+		printf("*");
+        
+		if(i==10)
+{
+			for(j=1;j<=6;j++) {
+     
+        			printf("*");   
+           
+			}
+        
+       
+		}
+     
+		printf("\n");
+    
+	}
+        
+   
+ 	return 0;
+
+}
